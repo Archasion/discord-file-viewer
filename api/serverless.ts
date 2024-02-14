34ad86c40aa23e41d30134ed0536dc6ff3f95d25
nick <59822256+Archasion@users.noreply.github.com>
2024-02-14 14:55:12 +0000

    test

--- api/serverless.ts
+++ api/serverless.ts
@@ -1,11 +1,10 @@
 import Fastify, { type FastifyReply, type FastifyRequest } from "fastify";
-import routes from "../src/app";
 
 const app = Fastify({
     logger: process.env.NODE_ENV === "development"
 });
 
-app.register(routes);
+app.register(import("../src/app"));
 
 export default async (req: FastifyRequest, res: FastifyReply) => {
     await app.ready();
