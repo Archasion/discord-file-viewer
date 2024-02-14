d56da8d9952a2ce80e183623904e936ebdb5465b
nick <59822256+Archasion@users.noreply.github.com>
2024-02-14 14:58:41 +0000

    test

--- api/serverless.ts
+++ api/serverless.ts
@@ -1,12 +1,26 @@
 import Fastify, { type FastifyReply, type FastifyRequest } from "fastify";
 
-const app = Fastify({
+const fastify = Fastify({
     logger: process.env.NODE_ENV === "development"
 });
 
-app.register(import("../src/app"));
+fastify.get("/", async (_req, res) => {
+    res.send("FORMAT: /attachments?url={encoded_url}");
+});
+
+fastify.get("/attachments", async (req: FastifyRequest<{ Querystring: Record<"url", string> }>, res) => {
+    try {
+        const url = decodeURIComponent(req.query.url);
+        const response = await fetch(url);
+        const content = await response.text();
+
+        res.send(content);
+    } catch {
+        res.send("Failed to fetch file content, please double-check the parameters");
+    }
+});
 
 export default async (req: FastifyRequest, res: FastifyReply) => {
-    await app.ready();
-    app.server.emit("request", req, res);
+    await fastify.ready();
+    fastify.server.emit("request", req, res);
 }
