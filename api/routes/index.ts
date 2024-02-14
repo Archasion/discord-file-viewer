f11ee62be5d8e973c5b8c489f4135a905efa409f
nick <59822256+Archasion@users.noreply.github.com>
2024-02-14 20:37:56 +0000

    commit

--- api/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import type { FastifyReply, FastifyRequest } from "fastify";
-
-export function index(_req: FastifyRequest, res: FastifyReply): void {
-    res.send("FORMAT: /files?url={encoded_url}");
-}
-
-export * from "./files.ts";
--- api/serverless.ts
+++ api/serverless.ts
@@ -1,8 +1,6 @@
 import Fastify, { type FastifyReply, type FastifyRequest } from "fastify";
 import fastifyRateLimit from "@fastify/rate-limit";
 
-import * as routes from "./routes/index.ts";
-
 // Create a Fastify instance
 const fastify = Fastify({
     logger: process.env.NODE_ENV === "development"
@@ -14,13 +12,33 @@ await fastify.register(fastifyRateLimit, {
     timeWindow: 1000 // 1 second
 });
 
-// Routes
-fastify.get("/", routes.index);
-fastify.get("/files", routes.fileViewer);
+// Index route
+fastify.get("/", (_req: FastifyRequest, res: FastifyReply) => {
+    res.send("FORMAT: /files?url={encoded_url}");
+});
+
+// File viewer route
+fastify.get("/files", async (req: FastifyRequest<FileViewerQueryParams>, res: FastifyReply) => {
+    try {
+        const url = decodeURIComponent(req.query.url);
+        const response = await fetch(url);
+        const content = await response.text();
+
+        res.send(content);
+    } catch {
+        res.send("Failed to fetch file content, please double-check the parameters");
+    }
+});
 
 // Start the server
 // noinspection JSUnusedGlobalSymbols
 export default async (req: FastifyRequest, res: FastifyReply): Promise<void> => {
     await fastify.ready();
     fastify.server.emit("request", req, res);
+}
+
+interface FileViewerQueryParams {
+    Querystring: {
+        url: string;
+    }
 }
